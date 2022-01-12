0309bb81fcb2f0a2f2777225e100eb05863b4ab8
Kevin Lockett <chlockett@msn.com>
2022-01-12 16:07:59 -0600

    kll-CHapter6 completed

--- aviary/birds.py
+++ aviary/birds.py
@@ -4,10 +4,17 @@ class Bird:
     """ Class to define instances of various animals in our petting zoo
     """
 
-    def __init__(self, name, type, species):
+    def __init__(self, name, type, species, food):
         # Establish the properties of each animal with a default value
         self.name = name
         self.type = type
         self.species = species
         self.flying = True
         self.date_added = date.today()
+        self.food = food
+        
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
+        
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
